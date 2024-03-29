c8af0d6d1bcabcc3efe8dd7f5f340e65be31fdde
Raghu2025 <raghun875@gmail.com>
2024-03-29 10:02:52 +0545

    test

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './src/app.module';
 
 export default async function handler(req, res) {
   const server = await NestFactory.create(AppModule);
