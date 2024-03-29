a4fed142506316686057c902399084b9a598ae40
Raghu2025 <raghun875@gmail.com>
2024-03-29 09:54:27 +0545

    vercel

---
+++ api/index.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+export default async function handler(req, res) {
+  const server = await NestFactory.create(AppModule);
+  server.enableCors();
+  const app = server.getHttpAdapter().getInstance();
+  await app.init();
+
+  const handler = app.getRequestHandler();
+  const result = await handler(req, res);
+
+  await app.close();
+  return result;
+}
